128d12715a21841e3c13a337c9119d8231edd4b6
Matthias Hryniszak <padcom@gmail.com>
2012-01-04 20:41:48 +0100

    Updated plugin version to better depict the project state

--- RestfulGormGrailsPlugin.groovy
+++ RestfulGormGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class RestfulGormGrailsPlugin {
     // the plugin version
-    def version = "1.0.0-SNAPSHOT"
+    def version = "0.0.1-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.0 > *"
     // the other plugins this plugin depends on
